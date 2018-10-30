1e9c2eefbe6b32d26c1c218f27345afd3ec76a0e
yxliaoyx <yxliaoyx@gmail.com>
2018-10-30 23:17:27 +0800

    Update sys_module.py

--- sys module/sys_module.py
+++ sys module/sys_module.py
@@ -1,7 +1,8 @@
+# 使用sys模組，讀入一連串整數(一列只有一個整數)直到EOF為止，輸出奇數的和與偶數的和
 import sys
 
 even, odd = 0, 0
-
+# 輸入多行直到Enter + Ctrl-Z
 for line in sys.stdin.readlines():
     int_line = int(line)
     if int_line % 2:
