4d543c19407d63dc99fed91ed14ac143a2c5dbc7
yxliaoyx <yxliaoyx@gmail.com>
2018-11-29 02:19:34 +0800

    Update csv_module.py
    ues DictReader

--- csv module/csv_module.py
+++ csv module/csv_module.py
@@ -8,26 +8,20 @@ class Point:
         self.y = y
 
 
-def convert(obj):
-    return {"x": obj.x, "y": obj.y}
-
-
 pt = [Point(np.random.randint(0, 1000) - 500, np.random.randint(0, 1000) - 500) for _ in range(100)]
 
 with open("points.csv", 'w', encoding='Big5', newline='') as csvfile:
     fieldnames = ['x', 'y']
 
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quotechar="'")
     writer.writeheader()
 
     for i in pt:
-        writer.writerow(convert(i))
+        writer.writerow({'x': i.x, 'y': i.y})
 
-pt2 = []
-with open("points.csv", 'r', encoding='Big5', newline='') as csvfile:  # encoding='utf-8'
-    reader = csv.reader(csvfile, delimiter=',', quotechar="'")
-    for row in reader:
-        pt2.append(Point(row[0], row[1]))
+with open("points.csv", 'r', encoding='Big5', newline='') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=',', quotechar="'")
+    pt2 = [Point(row['x'], row['y']) for row in reader]
 
 for p in pt2:
     print('x:{} y:{}'.format(p.x, p.y))
